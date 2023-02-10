9b2381d35de01d12cf7c16aab657c41f841f0637
kevinnail <fumalicious@gmail.com>
2023-02-10 12:49:48 -0800

    added getPostDetail fetch call

--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -1,17 +1,23 @@
 /* eslint-disable no-console */
 import { useState } from 'react';
 
-export default function PostForm({ title = '', description = '', submitHandler }) {
+export default function PostForm({ title = '', description = ' ', submitHandler }) {
   const [titleInput, setTitleInput] = useState(title);
   const [descriptionInput, setDescriptionInput] = useState(description);
 
   return (
     <div>
       <label>Title</label>
-      <input type="text" value={titleInput} onChange={(e) => setTitleInput(e.target.value)} />
+      <input
+        type="text"
+        name="title"
+        value={titleInput}
+        onChange={(e) => setTitleInput(e.target.value)}
+      />
       <label>Description</label>
       <input
         type="text"
+        name="description"
         value={descriptionInput}
         onChange={(e) => setDescriptionInput(e.target.value)}
       />
--- src/services/posts.js
+++ src/services/posts.js
@@ -14,3 +14,8 @@ export async function updatePost(id, title, description) {
   const resp = await client.from('posts').update({ title, description }).match({ id });
   return checkError(resp);
 }
+
+export async function getPostDetail(id) {
+  const resp = await client.from('posts').select('*').match({ id }).single();
+  return checkError(resp);
+}
