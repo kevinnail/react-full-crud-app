f541dba1f0358e09a1ba1690b9aaed456224c320
kevinnail <fumalicious@gmail.com>
2023-02-10 15:58:43 -0800

    updated button image and added delete button/ img

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
+import './PostCard.css';
 
 export default function PostCard({ title, description, user_id, id }) {
   const { user } = useUser();
@@ -9,8 +10,14 @@ export default function PostCard({ title, description, user_id, id }) {
       <h1>{title}</h1>
       <p>{description}</p>
       {owner && (
-        <p>
-          <Link to={`/posts/edit/${id}`}>Edit </Link>
+        <p className="button-container">
+          <span></span>
+          <Link className="edit-link" to={`/posts/edit/${id}`}>
+            <img src="/edit.png" className="edit-button" />{' '}
+          </Link>
+          <Link className="edit-link" to={`/posts/edit/${id}`}>
+            <img src="/delete.png" className="delete-button" />{' '}
+          </Link>
         </p>
       )}
     </div>
