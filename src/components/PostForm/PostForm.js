87a66190285aa8bb57255916ec065a65e5e8ad4f
kevinnail <fumalicious@gmail.com>
2023-02-14 16:06:47 -0800

    adapted new post form to submit upon pressing enter

--- src/components/PostForm/PostForm.js
+++ src/components/PostForm/PostForm.js
@@ -1,33 +1,37 @@
-/* eslint-disable no-console */
 import { useState } from 'react';
 
 export default function PostForm({ title = '', description = ' ', submitHandler }) {
   const [titleInput, setTitleInput] = useState(title);
   const [descriptionInput, setDescriptionInput] = useState(description);
 
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    submitHandler(titleInput, descriptionInput);
+  };
+
   return (
-    <div>
-      <label>Title</label>
-      <input
-        type="text"
-        name="title"
-        value={titleInput}
-        onChange={(e) => setTitleInput(e.target.value)}
-      />
-      <label>Description</label>
-      <input
-        type="text"
-        name="description"
-        value={descriptionInput}
-        onChange={(e) => setDescriptionInput(e.target.value)}
-      />
-      <button
-        onClick={() => {
-          submitHandler(titleInput, descriptionInput);
-        }}
-      >
-        Submit
-      </button>
-    </div>
+    <form onSubmit={handleFormSubmit}>
+      <div>
+        <label>Title</label>
+        <input
+          type="text"
+          name="title"
+          value={titleInput}
+          onChange={(e) => setTitleInput(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Description</label>
+        <input
+          type="text"
+          name="description"
+          value={descriptionInput}
+          onChange={(e) => setDescriptionInput(e.target.value)}
+        />
+      </div>
+      <div>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
   );
 }
